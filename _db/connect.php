7b3944aa84ba97035436a3b02a5ebfacc432b5c5
Eratsu <guilhermelins_muarena@live.com>
2014-07-06 11:48:30 -0300

    Remove db password :v:

--- _db/connect.php
+++ _db/connect.php
@@ -3,10 +3,10 @@
 $host = "localhost";
 $dbname = "otlist";
 $login = "root";
-$password = "guilazx123";
+$password = "";
 
 // $pdo = new PDO('mysql:host='.$host.';dbname='.$dbname.','.$login.','.$password.'');
-$pdo = new PDO("mysql:host=localhost;dbname=otlist","root","guilazx123");
+$pdo = new PDO("mysql:host=localhost;dbname=otlist","root","");
 if(!$pdo){
 	die("erro na conexao");
 }
